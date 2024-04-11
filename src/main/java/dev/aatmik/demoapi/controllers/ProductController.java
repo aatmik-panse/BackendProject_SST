3b19a24eabc82e811462995eb0bfc256b12b1d00
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-04-12 01:42:48 +0530

    error handling

--- src/main/java/dev/aatmik/demoapi/controllers/ProductController.java
+++ src/main/java/dev/aatmik/demoapi/controllers/ProductController.java
@@ -4,6 +4,8 @@ import dev.aatmik.demoapi.dtos.FakeStoreProductDTO;
 import dev.aatmik.demoapi.models.Product;
 import dev.aatmik.demoapi.service.FakeStoreProductService;
 import dev.aatmik.demoapi.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -32,8 +34,36 @@ public class ProductController { // waiter
     }
     //There are 2 methods of calling a endpoint  1./products/get/id 2./products/1 2nd as per RESTful API
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable("id") Long id) {
-        return  productService.getProductById(id);
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Long id) {
+
+//        throw new RuntimeException("Not implemented By Aatmik");
+        ResponseEntity<Product> responseEntity = null;
+        Product product = null;
+        try {
+            product = productService.getProductById(id);
+            responseEntity = new ResponseEntity<>(product, HttpStatus.NOT_FOUND);
+            System.out.println("got product by id");
+            return responseEntity;
+        } catch (RuntimeException exception) {
+            Object dto;
+            if (product == null) {
+                dto = new FakeStoreProductDTO();
+            } else {
+                dto = product;
+            }
+            new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
+    }
+    @GetMapping("/get/{id}")
+    public Product getProductById2(@PathVariable("id") Long id) {
+        try {
+            return productService.getProductById(id);
+
+        }catch (Exception e){
+            throw new RuntimeException("Not implemented By Aatmik");
+        }
+//        return  productService.getProductById(id);
     }
     @GetMapping("")
     public ArrayList<Product> getAllProducts() {
