7756b28122f8232ae14c3a322bcc3fd44adde56b
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-04-16 17:22:31 +0530

    dp version history using flyway

--- src/main/java/dev/aatmik/demoapi/service/SelfProductService.java
+++ src/main/java/dev/aatmik/demoapi/service/SelfProductService.java
@@ -1,5 +1,6 @@
 package dev.aatmik.demoapi.service;
 
+import dev.aatmik.demoapi.exception.CategoryNotFoundException;
 import dev.aatmik.demoapi.exception.ProductNotFoundException;
 import dev.aatmik.demoapi.models.Category;
 import dev.aatmik.demoapi.models.Product;
@@ -11,7 +12,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Optional;
 @Service
-@Primary
+@Primary // instead  of using @Primary we can use @Qualifier to specify which implementation to use
+         //in @qualifier we have to specify the name of the implementation as @Service("selfProductService") and in the controller we have to use @Qualifier("selfProductService")
 
 public class SelfProductService implements ProductService{
 
@@ -34,7 +36,7 @@ public class SelfProductService implements ProductService{
 
     @Override
     public ArrayList<Product> getAllProducts() {
-        return null;
+        return (ArrayList<Product>) productRepository.findAll(); //findAll() is a method of JpaRepository which returns a list of all the products
     }
 
     @Override
@@ -47,6 +49,9 @@ public class SelfProductService implements ProductService{
         }
         Product product1 = productRepository.save(product);
         Optional<Category> optionalCategory = categoryRepository.findById(category.getId());
+        if(optionalCategory.isEmpty()){
+            throw new CategoryNotFoundException(category.getId(),"Category not found");
+        }
         product1.setCategory(optionalCategory.get());
         return product1;
     }
